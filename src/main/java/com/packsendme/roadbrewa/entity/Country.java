90214343fdced74193c1c36d493a16b64c509869
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-11-17 15:29:32 +0100

    ChangeCountry

--- src/main/java/com/packsendme/roadbrewa/entity/Country.java
+++ src/main/java/com/packsendme/roadbrewa/entity/Country.java
@@ -2,6 +2,8 @@ package com.packsendme.roadbrewa.entity;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,17 +16,17 @@ public class Country implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public String idcountry;
+	@Id
+	public String id;
 	public String codcountry;
 	public String namecountry;
 	public String nameimagecountry;
 	public String formatnumbercountry;
 	
 	
-	public Country(String idcountry, String codcountry, String namecountry, String nameimagecountry,
+	public Country(String codcountry, String namecountry, String nameimagecountry,
 			String formatnumbercountry) {
 		super();
-		this.idcountry = idcountry;
 		this.codcountry = codcountry;
 		this.namecountry = namecountry;
 		this.nameimagecountry = nameimagecountry;
