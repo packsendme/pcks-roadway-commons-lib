1c56545e29f589807780e83621ace281ecde7890
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-11-18 16:26:28 +0100

    ChangeCountryAdd-UnityMeasurement

--- src/main/java/com/packsendme/roadbrewa/dto/CountryDto.java
+++ src/main/java/com/packsendme/roadbrewa/dto/CountryDto.java
@@ -20,21 +20,24 @@ public class CountryDto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	public String id;
+	public String idcountry;
 	public String codcountry;
 	public String namecountry;
 	public String nameimagecountry;
 	public String formatnumbercountry;
+	public List<String> unityMeasurementL = new ArrayList<String>();
 	
-	
-	public CountryDto(String codcountry, String namecountry, String nameimagecountry,
-			String formatnumbercountry) {
+
+	public CountryDto(String idcountry, String codcountry, String namecountry, String nameimagecountry,
+			String formatnumbercountry, List<String> unityMeasurementL) {
 		super();
+		this.idcountry = idcountry;
 		this.codcountry = codcountry;
 		this.namecountry = namecountry;
 		this.nameimagecountry = nameimagecountry;
 		this.formatnumbercountry = formatnumbercountry;
+		this.unityMeasurementL = unityMeasurementL;
 	}
-	
 
 	public CountryDto() {
 		super();
@@ -46,26 +49,34 @@ public class CountryDto implements Serializable {
 	 * ==============================================
 	 */
 	
-	public Country dtoTOentity(CountryDto country, Country entity, String typeOperation) {
+	public Country dtoTOentity(CountryDto countryDto, Country entity, String typeOperation) {
 		if(typeOperation.equals(RoadwayManagerConstants.ADD_OP_ROADWAY)) {
 			entity = new Country();
 		}
-		entity.codcountry = country.codcountry;
-		entity.namecountry = country.namecountry;
-		entity.nameimagecountry = country.nameimagecountry;
-		entity.formatnumbercountry = country.formatnumbercountry;
+		entity.idcountry = countryDto.idcountry;
+		entity.codcountry = countryDto.codcountry;
+		entity.namecountry = countryDto.namecountry;
+		entity.nameimagecountry = countryDto.nameimagecountry;
+		entity.formatnumbercountry = countryDto.formatnumbercountry;
+		if(countryDto.unityMeasurementL != null) {
+			entity.unityMeasurementL = countryDto.unityMeasurementL;
+		}
 		return entity;
 	}
 	
 	public List<CountryDto> entityTOdto(List<Country> country_L) {
 		List<CountryDto> countryDto_L = new ArrayList<CountryDto>();
 
-		for(Country country : country_L) {
+		for(Country entity : country_L) {
 			CountryDto countryDto = new CountryDto();
-			countryDto.codcountry = country.codcountry;
-			countryDto.namecountry = country.namecountry;
-			countryDto.nameimagecountry = country.nameimagecountry;
-			countryDto.formatnumbercountry = country.formatnumbercountry;
+			countryDto.idcountry = entity.idcountry;
+			countryDto.codcountry = entity.codcountry;
+			countryDto.namecountry = entity.namecountry;
+			countryDto.nameimagecountry = entity.nameimagecountry;
+			countryDto.formatnumbercountry = entity.formatnumbercountry;
+			if(entity.unityMeasurementL != null) {
+				countryDto.unityMeasurementL = entity.unityMeasurementL;
+			}
 			countryDto_L.add(countryDto);
 		}
 		return countryDto_L;
--- src/main/java/com/packsendme/roadbrewa/entity/Country.java
+++ src/main/java/com/packsendme/roadbrewa/entity/Country.java
@@ -1,6 +1,8 @@
 package com.packsendme.roadbrewa.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
@@ -18,20 +20,29 @@ public class Country implements Serializable {
 	
 	@Id
 	public String id;
+	public String idcountry;
 	public String codcountry;
 	public String namecountry;
 	public String nameimagecountry;
 	public String formatnumbercountry;
+	public List<String> unityMeasurementL = new ArrayList<String>();
 	
-	
-	public Country(String codcountry, String namecountry, String nameimagecountry,
-			String formatnumbercountry) {
+	public Country(String idcountry, String codcountry, String namecountry, String nameimagecountry,
+			String formatnumbercountry, List<String> unityMeasurementL) {
 		super();
+		this.idcountry = idcountry;
 		this.codcountry = codcountry;
 		this.namecountry = namecountry;
 		this.nameimagecountry = nameimagecountry;
 		this.formatnumbercountry = formatnumbercountry;
+		this.unityMeasurementL = unityMeasurementL;
 	}
+
+
+
+
+
+
 
 
 	public Country() {
