4cb3c47aa002aefb3aceee70aa556a436903e875
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2021-03-24 10:37:00 +0100

    ok

--- src/main/java/com/packsendme/roadbrewa/entity/TollsFuel.java
+++ src/main/java/com/packsendme/roadbrewa/entity/TollsFuel.java
@@ -16,9 +16,9 @@ public class TollsFuel implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	public String country;
-	public double tolls_price;
-	public double fuelGasoline_price;
-	public double fuelDiesel_price;
+	public Double tolls_price;
+	public Double fuelGasoline_price;
+	public Double fuelDiesel_price;
 	public String current;
 	public String dt_inc;
 	public String dt_update;
