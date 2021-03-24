67771be5e7cc58d6e3253942fb169dfd9f0afc75
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2021-03-24 10:26:06 +0100

    ok

--- src/main/java/com/packsendme/roadbrewa/entity/TollsFuel.java
+++ src/main/java/com/packsendme/roadbrewa/entity/TollsFuel.java
@@ -2,8 +2,6 @@ package com.packsendme.roadbrewa.entity;
 
 import java.io.Serializable;
 
-import org.springframework.data.annotation.Id;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,9 +16,9 @@ public class TollsFuel implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	public String country;
-	public Double tolls_price;
-	public Double fuelGasoline_price;
-	public Double fuelDiesel_price;
+	public double tolls_price;
+	public double fuelGasoline_price;
+	public double fuelDiesel_price;
 	public String current;
 	public String dt_inc;
 	public String dt_update;
